2cbe8cec90a0edf2a1b2540e76e5d83d851ba1c0
dmcneill15 <dvveijeren@gmail.com>
2024-10-03 17:54:27 +1300

    Minor change

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -6,7 +6,20 @@ if (!DB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-let cached = global.mongoose;
+// if the connection fails, try 127.0.0.1 instead oflocalhost below
+const uri = process.env.DB_URI;
+
+async function connectToDatabase() {
+  // Connect to MongoDB
+  mongoose.connect(uri)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(error => console.log('MongoDB Error:' + error.message));
+  // Get the default connection
+  const db = mongoose.connection;
+  // Bind connection to error event (to get notification ofconnection errors)
+  db.on("error", console.error.bind(console, "MongoDBconnection error:"));
+}
+/*let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
@@ -22,12 +35,12 @@ async function connectToDatabase() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(DB_URI, opts).then((mongoose) => {
+    cached.promise = await mongoose.connect(DB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
   cached.conn = await cached.promise;
   return cached.conn;
-}
+}*/
 
 export default connectToDatabase;
